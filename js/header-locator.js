6dd0d5d7c6c3dfb34307986f9411c05cc6134ac3
eilyy <eil1337off@gmail.com>
2021-10-18 15:13:40 +0300

    labs 2, 3, 4 done

---
+++ js/header-locator.js
@@ -0,0 +1,7 @@
+const navMenu = document.querySelector('.nav');
+if(document.location.pathname === '/itmo-web-labs/') {
+    navMenu.querySelector('#main-btn').classList.add('nav__link_current')
+}
+if(document.location.pathname === '/itmo-web-labs/contacts/') {
+    navMenu.querySelector('#contacts-btn').classList.add('nav__link_current')
+}
