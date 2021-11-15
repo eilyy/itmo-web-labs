134f7cf46bf60e2d578ce6e47c28fbcdc6fd17fe
eilyy <eil1337off@gmail.com>
2021-11-15 15:30:25 +0300

    lab5 few bugs fixed

--- js/note.js
+++ js/note.js
@@ -1,8 +1,9 @@
-const btn = document.querySelector('#button-addon2');
+const form = document.querySelector('.notes__input');
 const notes = document.querySelector('.notes__list');
 const input = document.querySelector('.form-control');
 
-btn.addEventListener("click", () => {
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
     const noteContent = input.value;
     const newNote = document.createElement('div');
     newNote.classList.add('note-item');
