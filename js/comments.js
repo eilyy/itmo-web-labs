a259e963e3cf8b2fc468e8b8d392fcefcc3162df
eilyy <eil1337off@gmail.com>
2021-12-13 16:22:22 +0300

    few small fixes

--- js/comments.js
+++ js/comments.js
@@ -2,37 +2,45 @@ window.addEventListener('DOMContentLoaded', async () => {
     const commentsBlock = document.querySelector('#comments_section');
 
     const randomId = Math.floor(Math.random() * 100 + 1);
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${randomId}/comments`);
+    try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${randomId}/comments`);
 
-    document.querySelector('.spinner-wrapper').style.display = 'none';
+        document.querySelector('.spinner-wrapper').style.display = 'none';
 
-    if(!response.ok) {
-        const newErr = document.createElement('div');
-        newErr.classList.add('error');
-        newErr.innerText = 'Что-то пошло не так';
-        commentsBlock.appendChild(newErr);
-        return;
-    }
+        if(!response.ok) {
+            const newErr = document.createElement('div');
+            newErr.classList.add('error');
+            newErr.innerText = 'Что-то пошло не так';
+            commentsBlock.appendChild(newErr);
+            return;
+        }
 
-    const comments = await response.json();
+        const comments = await response.json();
 
-    if(comments.length === 0) {
-        const newErr = document.createElement('div');
-        newErr.classList.add('error');
-        newErr.innerText = 'Что-то пошло не так';
-        commentsBlock.appendChild(newErr);
-        return;
-    }
+        if(comments.length === 0) {
+            const newErr = document.createElement('div');
+            newErr.classList.add('error');
+            newErr.innerText = 'Что-то пошло не так';
+            commentsBlock.appendChild(newErr);
+            return;
+        }
 
-    comments.forEach(com => {
-        const newCom = document.createElement('div');
-        newCom.classList.add('comment');
-        newCom.innerHTML =
-            `<div class="comment__author">
+        comments.forEach(com => {
+            const newCom = document.createElement('div');
+            newCom.classList.add('comment');
+            newCom.innerHTML =
+                `<div class="comment__author">
                 <div class="comment__name">${com['name']}</div>
                 <div class="comment__email">${com['email']}</div>
             </div>
             <div class="comment__content">${com['body']}</div>`;
-        commentsBlock.appendChild(newCom);
-    });
+            commentsBlock.appendChild(newCom);
+        });
+    } catch (e) {
+        document.querySelector('.spinner-wrapper').style.display = 'none';
+        const newErr = document.createElement('div');
+        newErr.classList.add('error');
+        newErr.innerText = 'Что-то пошло не так';
+        commentsBlock.appendChild(newErr);
+    }
 });
--- js/note.js
+++ js/note.js
@@ -1,6 +1,7 @@
-const form = document.querySelector('.notes__input');
+const form = document.querySelector('.notes__form');
 const notes = document.querySelector('.notes__list');
-const input = document.querySelector('.form-control');
+const input = document.querySelector('.notes__input');
+const button = document.querySelector('.notes__btn');
 
 const getNotesFromStorage = () => {
     if(localStorage.getItem('notes')) {
@@ -15,22 +16,55 @@ window.addEventListener('DOMContentLoaded', () => {
     notesFromStorage.forEach(note => {
         const newNote = document.createElement('div');
         newNote.classList.add('note-item');
-        newNote.innerText = note;
+        newNote.innerHTML = `<div class="note-content">${note}</div><div class="close-btn"><i class="fas fa-times"></i></div>`;
         notes.appendChild(newNote);
         input.value = '';
     });
+
+    const closeBtns = document.querySelectorAll('.close-btn');
+
+    closeBtns.forEach(closeBtn => {
+        closeBtn.addEventListener('click', () => {
+            const noteContent = closeBtn.previousSibling.textContent;
+            closeBtn.closest('.note-item').remove();
+            const notes = getNotesFromStorage();
+            const newNotes = notes.filter((item) => item !== noteContent);
+
+            localStorage.setItem('notes', JSON.stringify(newNotes));
+        });
+    });
+});
+
+input.addEventListener('input', () => {
+    button.disabled = input.value === '';
 });
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const noteContent = input.value;
+
+    if(noteContent === '') {
+        button.disabled = true;
+        return;
+    }
+
     const notesFromStorage = getNotesFromStorage();
     notesFromStorage.push(noteContent);
     const newNote = document.createElement('div');
     newNote.classList.add('note-item');
-    newNote.innerText = noteContent;
+    newNote.innerHTML = `<div class="note-content">${noteContent}</div><div class="close-btn"><i class="fas fa-times"></i></div>`;
+    const newNoteCloseBtn = newNote.querySelector('.close-btn');
+    newNoteCloseBtn.addEventListener('click', () => {
+        const noteContent = newNoteCloseBtn.previousSibling.textContent;
+        newNoteCloseBtn.closest('.note-item').remove();
+        const notes = getNotesFromStorage();
+        const newNotes = notes.filter((item) => item !== noteContent);
+
+        localStorage.setItem('notes', JSON.stringify(newNotes));
+    });
     notes.appendChild(newNote);
     input.value = '';
+    button.disabled = true;
 
     localStorage.setItem('notes', JSON.stringify(notesFromStorage));
 });
